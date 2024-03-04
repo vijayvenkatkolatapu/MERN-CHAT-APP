46cc55691e90178be01b2da80da9f28e9029aa31
vijay venkat <123466102+vijayvenkatkolatapu@users.noreply.github.com>
2024-03-04 23:14:03 +0530

    First Commit
    First Date: 04-03-2024

---
+++ backend/data/data.js
@@ -0,0 +1,110 @@
+const chats = [
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Vijay Venkat",
+        email: "kolatapuvijayvenkat@gmail.com",
+      },
+      {
+        name: "Anudeep",
+        email: "21102106@rmd.ac.in",
+      },
+    ],
+    _id: "617a077e18c25468bc7c4dd4",
+    chatName: "Vijay Venkat",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Sai Teja",
+        email: "21102111@rmd.ac.in",
+      },
+      {
+        name: "Vijay Venkat",
+        email: "kolatapuvijayvenkat@gmail.com",
+      },
+    ],
+    _id: "617a077e18c25468b27c4dd4",
+    chatName: "Sai Teja",
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Anudeep",
+        email: "21102106@rmd.ac.in",
+      },
+      {
+        name: "Vijay Venkat",
+        email: "kolatapuvijayvenkat@gmail.com",
+      },
+    ],
+    _id: "617a077e18c2d468bc7c4dd4",
+    chatName: "Anudeep",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Vijay Venkat",
+        email: "kolatapuvijayvenkat@gmail.com",
+      },
+      {
+        name: "Anudeep",
+        email: "21102106@rmd.ac.in",
+      },
+      {
+        name: "Sai Teja",
+        email: "21102111@rmd.ac.in",
+      },
+    ],
+    _id: "617a518c4081150716472c78",
+    chatName: "FULL STACK TEAM",
+    groupAdmin: {
+      name: "Vijay Venkat",
+      email: "kolatapuvijayvenkat@gmail.com",
+    },
+  },
+  {
+    isGroupChat: false,
+    users: [
+      {
+        name: "Tharun",
+        email: "tharun123@gmail.com",
+      },
+      {
+        name: "Vijay Venkat",
+        email: "kolatapuvijayvenkat@gmail.com",
+      },
+    ],
+    _id: "617a077e18c25468bc7cfdd4",
+    chatName: "Vijay Venkat",
+  },
+  {
+    isGroupChat: true,
+    users: [
+      {
+        name: "Tharun",
+        email: "tharun123@gmail.com",
+      },
+      {
+        name: "Vijay Venkat",
+        email: "kolatapuvijayvenkat@gmail.com",
+      },
+      {
+        name: "Sai Teja",
+        email: "21102111@rmd.ac.in",
+      },
+    ],
+    _id: "617a518c4081150016472c78",
+    chatName: "Chill Zone",
+    groupAdmin: {
+      name: "Sai Teja",
+      email: "21102111@rmd.ac.in",
+    },
+  },
+];
+
+module.exports= {chats};
