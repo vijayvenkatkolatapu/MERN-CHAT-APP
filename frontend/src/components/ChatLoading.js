06aaa13399c7bff61ffa91caa094f1d7cfd8cb1a
vijay venkat <123466102+vijayvenkatkolatapu@users.noreply.github.com>
2024-03-05 16:58:26 +0530

    Add files via upload

---
+++ frontend/src/components/ChatLoading.js
@@ -0,0 +1,25 @@
+import { Stack } from '@chakra-ui/layout'
+import { Skeleton } from '@chakra-ui/skeleton'
+import React from 'react'
+
+const ChatLoading = () => {
+  return (
+    <Stack>
+        <Skeleton height='20px' />
+        <Skeleton height='20px' />
+        <Skeleton height='20px' />
+        <Skeleton height='20px' />
+        <Skeleton height='20px' />
+        <Skeleton height='20px' />
+        <Skeleton height='20px' />
+        <Skeleton height='20px' />
+        <Skeleton height='20px' />
+        <Skeleton height='20px' />
+        <Skeleton height='20px' />
+        <Skeleton height='20px' />
+    </Stack>
+
+  )
+}
+
+export default ChatLoading
