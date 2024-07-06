9f340d926c2262c32e7434fc1e9f8ca88945ff6c
vijay venkat <123466102+vijayvenkatkolatapu@users.noreply.github.com>
2024-07-06 14:11:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const __dirname1 =  path.resolve();
 
 if(process.env.NODE_ENV === 'production'){
 
-    app.use(express.static(path.join(__dirname1,'/opt/build/repo/public')));
+    app.use(express.static(path.join(__dirname1,'tree/main/opt/build/repo/public')));
 
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
